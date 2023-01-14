6f6c426396ab76ef5e839f526d2e86ec35d80ef1
rvafaeis <rvafaeis@gmail.com>
2023-01-14 15:58:31 -0500

    Commit 2

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -4,5 +4,17 @@ bank_path=os.path.join("PyBank","Resources", "budget_data.csv")
 with open(bank_path) as csvfile:
     csvreader=csv.reader(csvfile,delimiter=",")
     header=next(csvreader)
+    months=[]
+    profits=[]
     for row in csvreader:
-        print(row)
+        months.append(row[0])
+        profits.append(int(row[1]))
+    total_number_of_months=len(months)
+    total_profits=sum(profits)
+    changes=[]
+    for i in range(1,len(profits)):
+        changes.append(profits[i]-profits[i-1])
+    average_change=sum(changes)/len(changes)
+    
+        
+        
