21116bcdc59211e38dcbd0c4b2eb658028e92e9c
rvafaeis <rvafaeis@gmail.com>
2023-01-16 20:43:09 -0500

    HW Completed

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -15,6 +15,34 @@ with open(bank_path) as csvfile:
     for i in range(1,len(profits)):
         changes.append(profits[i]-profits[i-1])
     average_change=sum(changes)/len(changes)
-    
-        
-        
+  
+ # Obtain the max and min of the the montly profit change list
+    max_increase_value = max(changes)
+    max_decrease_value = min(changes)
+
+# Correlate max and min to the proper month using month list and index from max and min
+# plus 1 at the end since month associated with change is the + 1 month or next month
+    max_increase_month = months[changes.index(max(changes)) + 1]
+    max_decrease_month = months[changes.index(min(changes)) + 1]
+
+# Print Statement
+# Print Statement
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {total_number_of_months}")
+print(f"Total Profits: ${total_profits}")
+print(f"Average Change: ${round(average_change,2)}")
+print(f"Greatest Increase in Profits: {max_increase_month} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {max_decrease_month} (${(str(max_decrease_value))})")
+#Financial Summary
+output_file = os.path.join("PyBank", "Analysis", "Financial_Analysis_Summary.txt")
+with open(output_file,"w") as file:
+  # Write methods to print to Financial_Analysis_Summary 
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("----------------------------\n")      
+    file.write(f"Total Months: {total_number_of_months}\n")
+    file.write(f"Total Profits:${total_profits}\n")
+    file.write(f"Average Change: ${round(average_change,2)}\n")
+    file.write(f"Greatest Increase in Profits: {max_increase_month} (${(str(max_increase_value))})\n")
+    file.write(f"Greatest Decrease in Profits: {max_decrease_month} (${(str(max_decrease_value))})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,5 +4,41 @@ poll_path=os.path.join("PyPoll","Resources", "election_data.csv")
 with open(poll_path) as csvfile:
     csvreader=csv.reader(csvfile,delimiter=",")
     header=next(csvreader)
+
+    votes=[]
+    candidates={}
+
     for row in csvreader:
-        print(row)
+        votes.append(row[2])
+
+    total_number_of_votes=len(votes)
+    total_candidates=list(candidates)
+    
+    for cand in votes:
+        if candidates.get(cand):
+            candidates[cand] += 1
+        else:
+            candidates[cand] = 1
+
+    print("Election Results")
+    print("------------")
+    print(f"Total Votes: {total_number_of_votes}")
+    print("------------")
+    for cand, votes in candidates.items():
+        print(f"{cand}: {round((votes/total_number_of_votes) *100,3) }% ({votes})")
+    print("------------")
+    print(f"Winner: {max(candidates, key=candidates.get)}")
+
+output_file = os.path.join("PyPoll", "Analysis", "Election_Results_Summary.txt")
+
+with open(output_file,"w") as file:
+
+# Write methods to print to Elections_Results_Summary 
+    file.write(f"Election Results\n")
+    file.write("------------\n")
+    file.write(f"Total Votes: {total_number_of_votes}\n")
+    file.write("------------\n")
+    for cand, votes in candidates.items():
+        file.write(f"{cand}: {round((votes/total_number_of_votes) *100,3) }% ({votes})\n")
+    file.write("------------\n")
+    file.write(f"Winner: {max(candidates, key=candidates.get)}")
